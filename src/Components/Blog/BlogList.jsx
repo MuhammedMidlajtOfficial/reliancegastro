69ab779765f3a271101724d7e0ff3e6c6e46b8aa
Naren <deadlyassassin2003@gmail.com>
2024-11-26 15:01:43 +0530

    Adding the live route instead of local

--- src/Components/Blog/BlogList.jsx
+++ src/Components/Blog/BlogList.jsx
@@ -76,7 +76,7 @@ const BlogList = () => {
 
     try {
       await axios.put(
-        `http://localhost:9000/api/v1/blog/${selectedBlog._id}`,
+        `https://relience-test-backend.onrender.com/api/v1/blog/${selectedBlog._id}`,
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
       );
@@ -117,7 +117,7 @@ const BlogList = () => {
       if (result.isConfirmed) {
         try {
           await axios.delete(
-            `http://localhost:9000/api/v1/blog/${_id}`
+            `https://relience-test-backend.onrender.com/api/v1/blog/${_id}`
           );
           setBlogList(blogList.filter((blog) => blog._id !== _id));
           setTotalRows(totalRows - 1);
